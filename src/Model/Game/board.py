0c5190e68fb9e7c301350112b10932ae9f96c204
Exyos <augeardw@gmail.com>
2020-08-27 19:07:13 +0200

    Comment class

--- src/Model/Game/board.py
+++ src/Model/Game/board.py
@@ -1,20 +1,27 @@
+'''
+Class Board: Manages cards in board player
+'''
 class Board:
     def __init__(self):
         self.deck = [0]*15
         self.placeCards = []
         self.current_pos = 0
-
+    
+    # Place a card on a deck place
     def setCard(self, card, pos):
         self.deck[pos] = card
-
+    
+    # Add a card to the deck
     def addCard(self, card):
         if(self.current_pos < 15):
             self.setCard(card, self.current_pos)
             self.current_pos += 1
 
+    # Add a place card
     def addPlaceCard(self, card):
         self.placeCards.append(card)
-
+    
+    # Display the board on shell
     def display(self):
         i = 0
         print("BOARD:")
